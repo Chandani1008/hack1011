779adbe591832cc9004066575f7269f130fa8295
singhchandani1008@gmail.com <singhchandani1008@gmail.com>
2019-10-11 21:44:37 +0530

    first commit

---
+++ lower.py
@@ -0,0 +1,11 @@
+name="ChaMAn KuMaR"
+
+print(len(name))     #12(length of string inluding spaces)
+
+print(name.lower()) #chaman kumar
+
+print(name.upper()) #CHAMAN KUMAR
+
+print(name.title())  #Chaman Kumar
+
+print(name.count("a"))
